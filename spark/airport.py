ba1618c199003c516ed21393f8c1f7df35077156
Shaan <shaanqaus@gmail.com>
2020-07-27 23:35:28 -0400

    ubuntu

--- spark/airport.py
+++ spark/airport.py
@@ -11,6 +11,68 @@ sc = SparkContext(conf=conf)
 sc.setLogLevel("WARN")
 ssc = StreamingContext(sc, n_secs)
 
+# Those are my fields
+fields = ("FlightDate", "AirlineID", "FlightNum", "Origin", "OriginCityName", "OriginStateName", "Dest", "DestCityName",
+          "DestStateName", "CRSDepTime", "DepDelay", "CRSArrTime", "ArrDelay", "Cancelled", "CancellationCode",
+          "Diverted", "CRSElapsedTime", "ActualElapsedTime", "AirTime", "Distance")
+
+# A namedtuple object
+Ontime = namedtuple('Ontime', fields)
+
+
+def split(line):
+    """Operator function for splitting a line with csv module"""
+    reader = csv.reader(StringIO(line))
+    return list(reader)
+
+
+def splitOne(line):
+    """Operator function for splitting a line with csv module"""
+    reader = csv.reader(StringIO(line))
+    return reader.next()
+
+
+def parse(rows):
+    """Parse multiple rows"""
+    return [parse_row(row) for row in rows]
+
+
+def parse_row(row):
+    """Parses a row and returns a named tuple"""
+
+    row[fields.index("FlightDate")] = datetime.datetime.strptime(row[fields.index("FlightDate")], DATE_FMT).date()
+    row[fields.index("AirlineID")] = int(row[fields.index("AirlineID")])
+    row[fields.index("FlightNum")] = int(row[fields.index("FlightNum")])
+
+    # cicle amoung scheduled times
+    for index in ["CRSDepTime", "CRSArrTime"]:
+        if row[fields.index(index)] == "2400":
+            row[fields.index(index)] = "0000"
+
+        # Handle time values
+        try:
+            row[fields.index(index)] = datetime.datetime.strptime(row[fields.index(index)], TIME_FMT).time()
+
+        except ValueError:
+            # raise Exception, "problem in evaluating %s" %(row[fields.index(index)])
+            row[fields.index(index)] = None
+
+    row[fields.index("Cancelled")] = bool(int(row[fields.index("Cancelled")]))
+    row[fields.index("Diverted")] = bool(int(row[fields.index("Diverted")]))
+
+    # handle cancellation code
+    if row[fields.index("CancellationCode")] == '"':
+        row[fields.index("CancellationCode")] = None
+
+    # `handle float values
+    for index in ["DepDelay", "ArrDelay", "CRSElapsedTime", "Distance", "ActualElapsedTime", "AirTime"]:
+        try:
+            row[fields.index(index)] = float(row[fields.index(index)])
+        except ValueError:
+            row[fields.index(index)] = None
+
+    return Ontime(*row)
+
 def parse(rows):
     """Parse multiple rows"""
     return [parse_row(row) for row in rows]
